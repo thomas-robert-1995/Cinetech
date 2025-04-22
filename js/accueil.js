1e046b0cf4a9882450d41a66f10d63e56b49f7f8
naaaaath <nathan.leleu@laplateforme.io>
2025-04-22 10:44:36 +0200

    redirection des films dans l'accueil

--- js/accueil.js
+++ js/accueil.js
@@ -1,58 +1,41 @@
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmI4ZWYwMDllZmNkY2I3YzI1N2FiMjczNjAyMDIzZSIsIm5iZiI6MTc0NDc5NDIwNC4xOTYsInN1YiI6IjY3ZmY3MjVjZWY1YWU2ODdjYmQ5OTZkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8d9ewudWxMsREcB4sIVlHEiy9o7xiAzh2M_ilgBldUM'
-  }
-};
+document.addEventListener("DOMContentLoaded", async () => {
+    const moviesContainer = document.querySelector(".movie-list");
+    const seriesContainer = document.querySelector(".series-list");
 
-const movieList = document.querySelector('.movie-list');
-const seriesList = document.querySelector('.series-list');
-const suggestionsSection = document.getElementById('suggestions');
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmI4ZWYwMDllZmNkY2I3YzI1N2FiMjczNjAyMDIzZSIsIm5iZiI6MTc0NDc5NDIwNC4xOTYsInN1YiI6IjY3ZmY3MjVjZWY1YWU2ODdjYmQ5OTZkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8d9ewudWxMsREcB4sIVlHEiy9o7xiAzh2M_ilgBldUM'
+        }
+    };
 
-// 👉 Affiche les films
-fetch('https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=1', options)
-  .then(res => res.json())
-  .then(data => {
-    data.results.slice(0, 8).forEach(movie => {
-      const card = createCard(movie.title, movie.poster_path);
-      movieList.appendChild(card);
-    });
+    // Récupérer les films du moment
+    try {
+        const moviesResponse = await fetch("https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&page=1", options);
+        const moviesData = await moviesResponse.json();
+        displayItems(moviesData.results, moviesContainer, "movie");
 
-    // Suggestions : ajouter quelques films
-    data.results.slice(0, 4).forEach(movie => {
-      const card = createCard(movie.title, movie.poster_path);
-      suggestionsSection.appendChild(card);
-    });
-  })
-  .catch(err => console.error(err));
+        const seriesResponse = await fetch("https://api.themoviedb.org/3/tv/popular?language=fr-FR&page=1", options);
+        const seriesData = await seriesResponse.json();
+        displayItems(seriesData.results, seriesContainer, "tv");
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+    }
 
-// 👉 Affiche les séries
-fetch('https://api.themoviedb.org/3/tv/popular?language=fr-FR&page=1', options)
-  .then(res => res.json())
-  .then(data => {
-    data.results.slice(0, 8).forEach(serie => {
-      const card = createCard(serie.name, serie.poster_path);
-      seriesList.appendChild(card);
-    });
-
-    // Suggestions : ajouter quelques séries
-    data.results.slice(0, 4).forEach(serie => {
-      const card = createCard(serie.name, serie.poster_path);
-      suggestionsSection.appendChild(card);
-    });
-  })
-  .catch(err => console.error(err));
-
-// 🔧 Fonction utilitaire pour créer une carte de film/série
-function createCard(title, posterPath) {
-  const div = document.createElement('div');
-  div.className = 'media-card';
-  div.style.margin = '10px';
-  div.style.textAlign = 'center';
-  div.innerHTML = `
-    <img src="https://image.tmdb.org/t/p/w300${posterPath}" alt="${title}" style="width:150px; border-radius:10px;" />
-    <p>${title}</p>
-  `;
-  return div;
-}
+    function displayItems(items, container, type) {
+        container.innerHTML = ""; // Vider le conteneur avant d'ajouter les éléments
+        items.slice(0, items.length - 4).forEach(item => { // Exclure les 4 derniers films
+            const div = document.createElement("div");
+            div.classList.add("item");
+            div.innerHTML = `
+                <img src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="${item.title || item.name}" class="img-fluid">
+            `;
+            div.addEventListener("click", () => {
+                // Redirection vers la page de description avec l'ID et le type
+                window.location.href = `description.html?id=${item.id}&type=${type}`;
+            });
+            container.appendChild(div);
+        });
+    }
+});
